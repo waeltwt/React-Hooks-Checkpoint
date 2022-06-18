5fb3a534c2d2f82df0d914ad0d571ff849776392
waeltwt <waeltouati09@gmail.com>
2022-06-18 22:02:55 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MovieList from './components/MovieList'
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <MovieList/>
+    </React.Fragment>
+  );
+}
+
+export default App;
